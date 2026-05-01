0f4cb77780d44e04a611ab05ba2361f04d0527c8
João <moreirajoaopaulo951@gmail.com>
2026-05-01 08:46:34 -0300

    utilizando encodeURIComponent

--- assets/js/brasil.js
+++ assets/js/brasil.js
@@ -1,21 +1,22 @@
 const apiKey = 'd321bd7d3c9449f198852415262303';
-const proxy = 'https://api.allorigins.win/raw?url='
+const proxy = 'https://api.allorigins.win/raw?url=';
+
 document.addEventListener('DOMContentLoaded', async () => {
-  const weatherSP = await fetch(`${proxy}https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=Sao Paulo&lang=pt`)
+  const weatherSP = await fetch(`${proxy}${encodeURIComponent(`https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=Sao Paulo&lang=pt`)}`)
   const apiSP = await weatherSP.json();
   document.querySelector('.imgSP').src = apiSP.current.condition.icon;
   document.querySelector('.cidadeSP').textContent = apiSP.location.region;
   document.querySelector('.celsiusSP').textContent = apiSP.current.temp_c + '°';
   document.querySelector('.condicaoSP').textContent = apiSP.current.condition.text;
 
-  const weatherRJ = await await fetch(`${proxy}https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=Rio de janeiro&lang=pt`)
+  const weatherRJ = await fetch(`${proxy}${encodeURIComponent(`https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=Rio de Janeiro&lang=pt`)}`)
   const apiRJ = await weatherRJ.json();
   document.querySelector('.imgRJ').src = apiRJ.current.condition.icon;
   document.querySelector('.cidadeRJ').textContent = apiRJ.location.region;
   document.querySelector('.celsiusRJ').textContent = apiRJ.current.temp_c + '°';
   document.querySelector('.condicaoRJ').textContent = apiRJ.current.condition.text;
 
-  const weatherMG = await fetch(`${proxy}https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=Minas Gerais&lang=pt`)
+  const weatherMG = await fetch(`${proxy}${encodeURIComponent(`https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=Minas Gerais&lang=pt`)}`)
   const apiMG = await weatherMG.json();
   document.querySelector('.imgMG').src = apiMG.current.condition.icon;
   document.querySelector('.cidadeMG').textContent = apiMG.location.region;
